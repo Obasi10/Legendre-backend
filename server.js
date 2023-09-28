5eaad76433a81423b4bd7dde5d6e64bd528fd01d
Obasi10 <142413098+Obasi10@users.noreply.github.com>
2023-09-28 03:18:25 +0100

    Update server.js8757

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(express.json())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
+  res.setHeader("Access-Control-Allow-Origin","*")
+  res.setHeader("Access-Control-Allow-Methods","POST, GET, PUT, DELETE")
+  res.setHeader("Access-Control-Allow-Headers","Content-Type")
   next()
 })
 // routes
