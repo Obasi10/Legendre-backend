b0bee184e9c2e0639a8f14bc586b8ab6f3434ed3
Obasi10 <142413098+Obasi10@users.noreply.github.com>
2023-09-30 04:10:27 +0100

    Update userController.js2345

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,7 @@ const loginUser = async (req, res) => {
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({email, token, name: user.name, description: user.description})
+    res.status(200).json({email, token, name: user.name, description: user.description, suscribe: user.suscribe})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
@@ -32,7 +32,7 @@ const signupUser = async (req, res) => {
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({email, token, name: user.name, description: user.description})
+    res.status(200).json({email, token, name: user.name, description: user.description, suscribe: user.suscribe})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
